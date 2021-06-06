b53f172477335f6333d7757e66941cf625d50098
nic <mbnic@icloud.com>
2021-06-05 23:28:55 -0400

    final

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,20 +1,6 @@
 package base;
 import java.util.Scanner;
 
-/*
-Create a program that prompts for your name and prints a greeting using your name.
-
-Example Output
-What is your name? Brian
-Hello, Brian, nice to meet you!
-Constraint
-Keep the input, string concatenation, and output separate.
-Challenges
-Write a new version of the program without using any variables.
-Write a version of the program that displays different greetings for different people.
-Write a version of the program that displays the greeting in a GUI window.
- */
-
 public class App {
     static Scanner in = new Scanner(System.in);
 
